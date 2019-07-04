ce6eaa1f64d85c9b4f21da0c308ff6dc78e53a8e
kamal <dbkamal@gmail.com>
2019-07-04 16:24:51 -0400

    update the port

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     forecastMessage.textContent = '';
 
     const address = searchElement.value;
-    const url = 'http://localhost:3000/weather?address=' + address;
+    const url = '/weather?address=' + address;
 
     fetch(url).then((response) => {
         response.json().then((data) => {
